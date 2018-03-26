9dc403cbed83788f6df3aa2273880f1530880196
Ben Brooks <ben.brooks@couchbase.com>
2018-03-26 18:51:31 +0100

    Add stubs for new logging methods

---
+++ base/logging2.go
@@ -0,0 +1,27 @@
+package base
+
+// Errorf logs the given formatted string and args to the error log level with an optional log key.
+func Errorf(logKey uint32, format string, args ...interface{}) {
+	logTo(LevelError, logKey, format, args...)
+}
+
+// Warnf logs the given formatted string and args to the warn log level with an optional log key.
+func Warnf(logKey uint32, format string, args ...interface{}) {
+	logTo(LevelWarn, logKey, format, args...)
+}
+
+// Infof logs the given formatted string and args to the info log level with an optional log key.
+func Infof(logKey uint32, format string, args ...interface{}) {
+	logTo(LevelWarn, logKey, format, args...)
+}
+
+// Debugf logs the given formatted string and args to the debug log level with an optional log key.
+func Debugf(logKey uint32, format string, args ...interface{}) {
+	logTo(LevelWarn, logKey, format, args...)
+}
+
+// TODO: How to maintain log order consistency? Lock this function?
+func logTo(level uint32, logKey uint32, format string, args ...interface{}) {
+	// FIXME: Stub
+	return
+}
